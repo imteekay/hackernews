e7a28c8ab2765d4b7dc917959a18c6d4e0c093ed
= <leandrotk100@gmail.com>
2021-08-29 18:22:47 -0300

    Basic fetch

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,50 @@
+import { useState, useEffect } from 'react';
+
+const HACKER_NEWS_BASE_URL = 'https://hacker-news.firebaseio.com/v0';
+
+const useTopStories = () => {
+  const [topStories, setTopStories] = useState([]);
+
+  useEffect(() => {
+    const fetchHN = async () => {
+      const response = await fetch(`${HACKER_NEWS_BASE_URL}/topstories.json`);
+      const data = await response.json();
+      setTopStories(data.slice(0, 100));
+    };
+
+    fetchHN();
+  }, []);
+
+  return { topStories };
+};
+
+const useTopStoriesInfo = (topStories) => {
+  const [topStoriesInfo, setTopStoriesInfo] = useState([]);
+
+  useEffect(() => {
+    const fetchTopStories = async () => {
+      const responses = await Promise.all(
+        topStories.map((topStory) =>
+          fetch(`${HACKER_NEWS_BASE_URL}/item/${topStory}.json`),
+        ),
+      );
+
+      for (const response of responses) {
+        const data = await response.json();
+        setTopStoriesInfo((topStoriesInfo) => [...topStoriesInfo, data]);
+      }
+    };
+
+    fetchTopStories();
+  }, [topStories]);
+
+  return { topStoriesInfo };
+};
+
 export default function Home() {
+  const { topStories } = useTopStories();
+  const { topStoriesInfo } = useTopStoriesInfo(topStories);
+  console.log('topStoriesInfo', topStoriesInfo);
+
   return <h1>Home</h1>;
 }
