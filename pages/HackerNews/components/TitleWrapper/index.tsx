e120326769fcc990c81b60e2681c920136f582fd
= <leandrotk100@gmail.com>
2021-09-01 09:56:15 -0300

    Break url text when device is tablet or mobile

--- pages/HackerNews/components/TitleWrapper/index.tsx
+++ pages/HackerNews/components/TitleWrapper/index.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent } from 'react';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
-const titleWrapperStyle = {
-  display: 'flex',
+const getTitleWrapperStyle = (isMdUp: boolean) =>
+  isMdUp
+    ? {
+        display: 'flex',
+      }
+    : {};
+
+export const TitleWrapper: FunctionComponent = ({ children }) => {
+  const isMdUp = useMediaQuery('(min-width: 800px)');
+  return <div style={getTitleWrapperStyle(isMdUp)}>{children}</div>;
 };
-
-export const TitleWrapper: FunctionComponent = ({ children }) => (
-  <div style={titleWrapperStyle}>{children}</div>
-);
